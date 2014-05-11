0a4ce0357c2f035e9bd7baf544df10bcf254ccb4
Enrico Migliorini <enricomigliorini93@gmail.com>
2014-05-11 21:40:09 +0200

    removed undesired changes

--- build/battleSnake.build/Release/battleSnake.build/Objects-normal/x86_64/Item.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Enrico/Documents/Sviluppo/battleSnake/battleSnake/Item.cpp \
-  /Users/Enrico/Documents/Sviluppo/battleSnake/battleSnake/Item.h
--- build/battleSnake.build/Release/battleSnake.build/Objects-normal/x86_64/Tile.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Enrico/Documents/Sviluppo/battleSnake/battleSnake/Tile.cpp \
-  /Users/Enrico/Documents/Sviluppo/battleSnake/battleSnake/Tile.h
